6f501a46434a12967bfbdf0483f5fa1697c3ce9e
HuyenTrang <dangmaihuyentrang@gmail.com>
2026-04-21 21:01:24 +0700

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ fitmecentermanagement/src/main/java/com/example/fitmecenter/FitmecentermanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitmecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmecentermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitmecentermanagementApplication.class, args);
+	}
+
+}
---
+++ fitmecentermanagement/src/test/java/com/example/fitmecenter/FitmecentermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitmecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitmecentermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
